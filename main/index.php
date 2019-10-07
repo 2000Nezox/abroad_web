57af20d4652bbde1076c3cb7d695b6cd3fabc45e
ganngiNote <ganngi77@gmail.com>
2019-10-07 12:40:25 +0900

    メインページの大枠を作成しました

---
+++ main/index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>$Title$</title>
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/main_index.css">
+    <script src="../js/jquery-3.4.1.min.js"></script>
+</head>
+<body>
+    <?php include ("../sharedfile/header.php");?>
+<!--    <ul>-->
+<!--        <li>学生</li>-->
+<!--        <li>管理者</li>-->
+<!--    </ul>-->
+<!---->
+<!--    <table>-->
+        <!--- ここはjsでDBから動的に習得する --->
+<!--        <tr>-->
+<!--            <th>学籍番号</th>-->
+<!--            <th>学校名</th>-->
+<!--            <th>担任名</th>-->
+<!--            <th>学年</th>-->
+<!--            <th>名前</th>-->
+<!--            <th>なまえ</th>-->
+<!--            <th>留学予定</th>-->
+<!--        </tr>-->
+<!--        <tr>-->
+<!--            <td>-->
+<!--                <select>-->
+<!--                    <option value="ascendingOrder">昇順ソート</option>-->
+<!--                    <option value="descSort">降順ソード</option>-->
+<!--                </select>-->
+<!--            </td>-->
+            <!--- ここはDBから動的に生成するべきかも --->
+<!--            <td>-->
+<!--                <select>-->
+<!--                    <option value="">---</option>-->
+<!--                </select>-->
+<!--            </td>-->
+<!--            <td>-->
+<!--                <select>-->
+<!--                    <option value="">---</option>-->
+<!--                </select>-->
+<!--            </td>-->
+<!--            <td>-->
+<!--                <select>-->
+<!--                    <option value="">---</option>-->
+<!--                </select>-->
+<!--            </td>-->
+<!--            <td>-->
+<!--                <select>-->
+<!--                    <option value="">---</option>-->
+<!--                </select>-->
+<!--            </td>-->
+<!--            <td>-->
+<!--                <select>-->
+<!--                    <option value="">---</option>-->
+<!--                </select>-->
+<!--            </td>-->
+<!--            <td>-->
+<!--                <select>-->
+<!--                    <option value="">---</option>-->
+<!--                </select>-->
+<!--            </td>-->
+<!--        </tr>-->
+<!--    </table>-->
+<!--    <table>-->
+<!---->
+        <!--- ここはphpでデータベースから持ってきて埋め込む  --->
+        <!--- 以下イメージ、上記の機能が完了後は削除 --->
+<!--        <tr>-->
+<!--            <td>1801017</td>-->
+<!--            <td>麻生情報ビジネス専門学校</td>-->
+<!--            <td>情報システム専攻科</td>-->
+<!--            <td>久家まさと</td>-->
+<!--            <td>2年</td>-->
+<!--            <td>酒井　春華</td>-->
+<!--            <td>サカイ　ハルカ</td>-->
+<!--            <td>あり</td>-->
+<!--        </tr>-->
+<!--    </table>-->
+<!--    <button>CSV出力</button>-->
+
+</body>
+</html>
---
+++ sharedfile/header.php
@@ -0,0 +1,16 @@
+<header id="site_heder">
+    <div>
+        <h1>ASO ENGLISH +</h1>
+        <p class="header-float-left">by KUGA Tech</p>
+    </div>
+    <nav>
+        <ul class="foo">
+            <li>学生</li>
+            <li>講座</li>
+            <li>通知</li>
+            <li>面談</li>
+            <li>設定</li>
+        </ul>
+    </nav>
+
+</header>
