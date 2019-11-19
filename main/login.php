c21ccfe652e554caf910e935f147d9648386bf21
ganngiNote <ganngi77@gmail.com>
2019-11-19 14:38:54 +0900

    もうやりたくない

---
+++ main/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>ログインページ</title>
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/header.css">
+    <link rel="stylesheet" href="../css/setting_list.css">
+    <link rel="stylesheet" href="../css/login.css">
+    <script src="../js/jquery-3.4.1.min.js"></script>
+</head>
+<diV class="login-form">
+    <h1>ASO English +</h1>
+    <p>by KUGA Tech</p>
+    <form method="post" action="user.php">
+        <div class="login-form1">
+            <input class="h" type="text" name="name" value="" placeholder="教職員番号">
+            <input class="h" type="text" name="name" value="" placeholder="パスワード">
+        </div>
+            <input id="login" type="submit" name ="check" value = "ログイン">
+    </form>
+</diV>
+</body>
+</html>
--- sharedfile/setting_list.php
+++ sharedfile/setting_list.php
@@ -5,7 +5,7 @@
                 <li id="sub-heder1"><a href="../main/setting_top.php">プロフィール設定</a></li>
                 <li id="sub-heder2"><a href="../main/setting_study_abroad_detail.php">留学先設定</a></li>
                 <li id="sub-heder3"><a href="../main/setting_img.php">留学支援画面設定</a></li>
-                <li id="sub-heder4"><a href="../main/setting_course_history.php">過去の口座</a></li>
+                <li id="sub-heder4"><a href="../main/setting_course_history.php">過去の講座</a></li>
             </ul>
         </nav>
     </div>
