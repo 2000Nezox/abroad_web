09d4bd7c68fafe5c4807fdb2ea41651688059ca3
ganngiNote <ganngi77@gmail.com>
2019-12-10 19:47:20 +0900

    index.phpを追加しました

--- class/Controll/account/login/LoginConfirmation.php
+++ class/Controll/account/login/LoginConfirmation.php
@@ -4,12 +4,11 @@ class LoginConfirmation
 {
     public static function Confirmation()
     {
-
         if (!empty($_SESSION["user_id"])) {
             return;
         } else {
             print_r("ログインしていません");
-            header('location: index.php');
+            header('location: login.php');
             exit();
         }
     }
